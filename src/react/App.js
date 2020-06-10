b23fb50bdf140cfb84e269417e8df79988ca9759
Jalal Belsifar <jalal.belsifar@gmail.com>
2020-06-10 11:02:10 -0500

    Commit changes to app.js and registration with the help with Leanne

--- src/react/App.js
+++ src/react/App.js
@@ -1,16 +1,18 @@
-import React from "react";
-import { Switch, Route } from "react-router-dom";
-import Home from "./Home";
-import Profile from "./Profile";
-import NotFound from "./NotFound";
-
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Home from './Home';
+import Profile from './Profile';
+import NotFound from './NotFound';
+import Register from './components/Register';
+import 'semantic-ui-css/semantic.min.css';
 class App extends React.Component {
   render() {
     return (
       <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/profiles/:username" component={Profile} />
-        <Route path="*" component={NotFound} />
+        <Route exact path='/' component={Home} />
+        <Route exact path='/profiles/:username' component={Profile} />
+        <Route path='*' component={NotFound} />
+        <Route exact path='/signup-form' component={Register} />
       </Switch>
     );
   }
--- src/react/components/LoginForm.js
+++ src/react/components/LoginForm.js
@@ -1,18 +1,19 @@
-import React from "react";
-import Spinner from "react-spinkit";
-import { connect } from "react-redux";
-import { login } from "../../redux";
-import "./LoginForm.css";
-
+import React from 'react';
+import Spinner from 'react-spinkit';
+import { connect } from 'react-redux';
+import { login } from '../../redux';
+import './LoginForm.css';
+import { Button, Form } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
 class LoginForm extends React.Component {
-  state = { username: "", password: "" };
+  state = { username: '', password: '' };
 
-  handleLogin = e => {
+  handleLogin = (e) => {
     e.preventDefault();
     this.props.login(this.state);
   };
 
-  handleChange = e => {
+  handleChange = (e) => {
     this.setState({ [e.target.name]: e.target.value });
   };
 
@@ -20,38 +21,49 @@ class LoginForm extends React.Component {
     const { loading, error } = this.props;
     return (
       <React.Fragment>
-        <form id="login-form" onSubmit={this.handleLogin}>
-          <label htmlFor="username">Username</label>
-          <input
-            type="text"
-            name="username"
-            autoFocus
-            required
-            onChange={this.handleChange}
-          />
-          <label htmlFor="password">Password</label>
-          <input
-            type="password"
-            name="password"
-            required
-            onChange={this.handleChange}
-          />
-          <button type="submit" disabled={loading}>
-            Login
-          </button>
-        </form>
-        {loading && <Spinner name="circle" color="blue" />}
-        {error && <p style={{ color: "red" }}>{error.message}</p>}
+        <div className='login-form-wrapper'>
+          <h2>Kwitter for you Quiters.</h2>
+          <Form id='login-form' onSubmit={this.handleLogin}>
+            <Form.Field>
+              <label>Username</label>
+              <input
+                type='text'
+                name='username'
+                autoFocus
+                placeholder='Username'
+                required
+                onChange={this.handleChange}
+              />
+            </Form.Field>
+            <Form.Field>
+              <label>Password</label>
+              <input
+                type='password'
+                placeholder='Password'
+                name='password'
+                required
+                onChange={this.handleChange}
+              />
+            </Form.Field>
+            <Button type='submit' disabled={loading}>
+              Login
+            </Button>
+            <br />
+            <Link to='/signup-form'> Sign Up for Kwitter!</Link>
+          </Form>
+          {loading && <Spinner name='circle' color='blue' />}
+          {error && <p style={{ color: 'red' }}>{error.message}</p>}
+        </div>
       </React.Fragment>
     );
   }
 }
 
 export default connect(
-  state => ({
+  (state) => ({
     result: state.auth.login.result,
     loading: state.auth.login.loading,
-    error: state.auth.login.error
+    error: state.auth.login.error,
   }),
   { login }
 )(LoginForm);
