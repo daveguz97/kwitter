1c99a5e84e68a9f860fbb3c6e6a39bbe7127b844
Jalal Belsifar <jalal.belsifar@gmail.com>
2020-06-10 15:30:24 -0500

    commit chnages to register and moved it outside the component, now the registration works

--- src/react/App.js
+++ src/react/App.js
@@ -3,7 +3,7 @@ import { Switch, Route } from 'react-router-dom';
 import Home from './Home';
 import Profile from './Profile';
 import NotFound from './NotFound';
-import Register from './components/Register';
+import Register from './Register';
 import 'semantic-ui-css/semantic.min.css';
 class App extends React.Component {
   render() {
@@ -11,8 +11,8 @@ class App extends React.Component {
       <Switch>
         <Route exact path='/' component={Home} />
         <Route exact path='/profiles/:username' component={Profile} />
-        <Route path='*' component={NotFound} />
         <Route exact path='/register-form' component={Register} />
+        <Route path='*' component={NotFound} />
       </Switch>
     );
   }
--- src/react/Register.js
+++ src/react/Register.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import { RegistrationForm, Menu } from '.';
-
+import { RegistrationForm, Menu } from '../react/components/index';
 class Register extends React.Component {
   render() {
     return (
       <React.Fragment>
         <Menu></Menu>
-        <RegistrationForm></RegistrationForm>
+        <RegistrationForm />
       </React.Fragment>
     );
   }
