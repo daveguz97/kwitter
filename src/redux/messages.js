a77128cc1e3c5250e75dd66260368297c1602449
Jasmyne Ford <pauldingpawn@Pauldings-MBP.attlocal.net>
2020-06-12 19:53:36 -0400

    updated messages.js

--- src/redux/messages.js
+++ src/redux/messages.js
@@ -8,29 +8,29 @@ import {
     createReducer,
   } from './helpers';
 
+  const url = domain + "/messages";
 
-const CREATE_MESSAGE = createActions('createMessage');
-export const createMessage = (createMessageData) => (dispatch, getState) => {
-  dispatch(CREATE_MESSAGE.START());
-
-  const { username, token } = getState().auth.login.result;
-
-  return fetch(domain + `/users/${username}`, {
-    method: 'POST',
-    headers: { Authorization: 'Bearer ' + token, ...jsonHeaders },
-  })
-    .then(handleJsonResponse)
-    .then((result) => dispatch(CREATE_MESSAGE.SUCCESS(result)))
-    .catch((err) => Promise.reject(dispatch(GET_IMAGE_PRO.FAIL(err))))
-    .then((result) => dispatch(LOGOUT.SUCCESS(result)));
-};
+  const CREATE_MESSAGE = createActions("createMessage");
+  export const CREATE_MESSAGE = createMessageData => dispatch => {
+    dispatch(LOGIN.START());
+  
+    return fetch(url + "/message", {
+      method: "POST",
+      headers: jsonHeaders,
+      body: JSON.stringify(createMessageData)
+    })
+      .then(handleJsonResponse)
+      .then(result => dispatch(LOGIN.SUCCESS(result)))
+      .catch(err => Promise.reject(dispatch(LOGIN.FAIL(err))));
+  };
+  
 const DELETE_MESSAGE = createActions('deleteMessage');
 export const deleteMessage = (deleteMessageData) => (dispatch, getState) => {
   dispatch(DELETE_MESSAGE.START());
 
   const { username, token } = getState().auth.login.result;
 
-  return fetch(domain + `/users/${username}`, {
+  return fetch(domain + `/message/${username}`, {
     method: 'DELETE',
     headers: { Authorization: 'Bearer ' + token, ...jsonHeaders },
   })
@@ -45,13 +45,13 @@ export const messageList = (messageListData) => (dispatch, getState) => {
 
   const { username, token } = getState().auth.login.result;
 
-  return fetch(domain + `/users/${username}`, {
-    method: 'DELETE',
+  return fetch(domain + `/message/${username}`, {
+    method: 'GET',
     headers: { Authorization: 'Bearer ' + token, ...jsonHeaders },
   })
     .then(handleJsonResponse)
     .then((result) => dispatch(MESSAGE_LIST.SUCCESS(result)))
-    .catch((err) => Promise.reject(dispatch(GET_IMAGE_PRO.FAIL(err))))
+    .catch((err) => Promise.reject(dispatch(MESSAGE_LIST.FAIL(err))))
     .then((result) => dispatch(LOGOUT.SUCCESS(result)));
 };
 export const reducers = {
