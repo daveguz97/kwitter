1164ee625c4296f7d8eb79672851845f9562d2bc
David Guzman <davidguzmaninc@gmail.com>
2020-06-12 16:17:28 -0400

    deleted option for light and dark mode for now

--- src/react/Home.js
+++ src/react/Home.js
@@ -4,39 +4,10 @@ import { userIsNotAuthenticated } from "./HOCs";
 import Button from "react-bootstrap/Button";
 import "./Home.scss";
 class Home extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { darkMode: false };
-  }
-
-  setDarkMode = () => {
-    this.setState({
-      darkMode: true,
-    });
-  };
-
-  setLightMode = () => {
-    this.setState({
-      darkMode: false,
-    });
-  };
-
   render() {
     return (
-      <div className={`Home ${this.state.darkMode ? "darkmode" : "lightmode"}`}>
-        <div className="options">
-          <Button id="dark-mode-btn" variant="dark" onClick={this.setDarkMode}>
-            Dark Mode
-          </Button>
-          <Button
-            id="light-mode-btn"
-            variant="light"
-            onClick={this.setLightMode}
-          >
-            Light Mode
-          </Button>
-        </div>
-        <LoginForm darkMode={this.state.dark} />
+      <div className="Home">
+        <LoginForm />
       </div>
     );
   }
