6c88e54035c9a678956390791dcaf3153800221b
David Guzman <davidguzmaninc@gmail.com>
2020-06-14 19:41:53 -0400

    Added default avatar picture

--- src/react/components/NavigationBar.js
+++ src/react/components/NavigationBar.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 // import "./NavigationBar.css";
-import { connect } from 'react-redux';
-import { logout } from '../../redux';
-import { Menu, Image } from 'semantic-ui-react';
-// import logo from './kwitter_logo_small.jpg';
+import { connect } from "react-redux";
+import { logout } from "../../redux";
+import { Menu, Image } from "semantic-ui-react";
+import Kwitterlogo from "../img/kwitter-logo.png";
 
 class NavigationBar extends React.Component {
-  state = { activeItem: 'Profile' };
+  state = { activeItem: "Profile" };
   handleLogout = (e) => {
     e.preventDefault();
     this.props.logout();
@@ -19,39 +19,39 @@ class NavigationBar extends React.Component {
     const { activeItem } = this.state;
 
     return (
-      <div id='menu'>
-        <h1 id='title'>Kwitter</h1>
-        <div className='logo-wrapper'>
+      <div id="menu">
+        <h1 id="title">Kwitter</h1>
+        <div className="logo-wrapper">
           <Image
-            // src={logo}
-            size='small'
-            alt=''
-            className='ui-small-image'
+            src={Kwitterlogo}
+            size="small"
+            alt=""
+            className="ui-small-image"
             circular
           />
         </div>
         <br />
-        <div id='menu-links'>
+        <div id="menu-links">
           {this.props.isAuthenticated && (
             <Menu pointing secondary vertical>
-              <Link to='/profiles/:username'>
+              <Link to="/profiles/:username">
                 <Menu.Item
-                  name='Profile'
-                  active={activeItem === 'Profile'}
+                  name="Profile"
+                  active={activeItem === "Profile"}
                   onClick={this.handleItemClick}
                 />
               </Link>
-              <Link to='/messagefeed'>
+              <Link to="/messagefeed">
                 <Menu.Item
-                  name='Message Feed'
-                  active={activeItem === 'Message Feed'}
+                  name="Message Feed"
+                  active={activeItem === "Message Feed"}
                   onClick={this.handleItemClick}
                 />
               </Link>
-              <Link to='/' onClick={this.handleLogout}>
+              <Link to="/" onClick={this.handleLogout}>
                 <Menu.Item
-                  name='Logout'
-                  active={activeItem === 'Logout'}
+                  name="Logout"
+                  active={activeItem === "Logout"}
                   onClick={this.handleItemClick}
                 />
               </Link>
