6d5a20c5a0c33a8604cbc2d58e43dd5b0bd04cc0
Jalal Belsifar <jalal.belsifar@gmail.com>
2020-06-14 07:45:09 -0500

    changes to master

--- src/react/components/LoginForm.js
+++ src/react/components/LoginForm.js
@@ -1,21 +1,21 @@
-import React from "react";
-import Menu from "./Menu";
-import Spinner from "react-spinkit";
-import { connect } from "react-redux";
-import { login } from "../../redux";
-import { Link } from "react-router-dom";
-import KwitterLogo from "../img/kwitter-logo.png";
-import Button from "react-bootstrap/Button";
-import Container from "react-bootstrap/Container";
-import FormGroup from "react-bootstrap/FormGroup";
-import FormControl from "react-bootstrap/FormControl";
-import FormText from "react-bootstrap/FormText";
-import GoogleLogin from "react-google-login";
-import kwitterLogo from "../img/kwitter-logo.png";
-import "./LoginForm.scss";
+import React from 'react';
+import Menu from './Menu';
+import Spinner from 'react-spinkit';
+import { connect } from 'react-redux';
+import { login } from '../../redux';
+import { Link } from 'react-router-dom';
+import KwitterLogo from '../img/kwitter-logo.png';
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import FormGroup from 'react-bootstrap/FormGroup';
+import FormControl from 'react-bootstrap/FormControl';
+import FormText from 'react-bootstrap/FormText';
+// import GoogleLogin from "react-google-login";
+import kwitterLogo from '../img/kwitter-logo.png';
+import './LoginForm.scss';
 
 class LoginForm extends React.Component {
-  state = { username: "", password: "" };
+  state = { username: '', password: '' };
 
   handleLogin = (e) => {
     e.preventDefault();
@@ -29,54 +29,54 @@ class LoginForm extends React.Component {
   render() {
     const { loading, error } = this.props;
     return (
-      <section className="LoginForm my-auto text-center">
-        <aside className="left-side">
+      <section className='LoginForm my-auto text-center'>
+        <aside className='left-side'>
           <Menu darkMode={this.state.darkMode} />
-          <div className="text-center">
-            <img src={kwitterLogo} alt="Logo" className="logo" />
+          <div className='text-center'>
+            <img src={kwitterLogo} alt='Logo' className='logo' />
             <h1>Find Out What's happening</h1>
             <ul>
               <li>
-                <i className="fas fa-user-friends"></i> Gain a Fan Base
+                <i className='fas fa-user-friends'></i> Gain a Fan Base
               </li>
               <li>
-                <i className="far fa-comment"></i> Post New Messages
+                <i className='far fa-comment'></i> Post New Messages
               </li>
               <li>
-                <i className="far fa-newspaper"></i> See the latest trends and
+                <i className='far fa-newspaper'></i> See the latest trends and
                 news
               </li>
             </ul>
           </div>
         </aside>
-        <Container className="mx-auto">
-          <img className="logo" src={KwitterLogo} alt="Kwitter Logo" />
+        <Container className='mx-auto'>
+          <img className='logo' src={KwitterLogo} alt='Kwitter Logo' />
           <h1>Sign In</h1>
-          <form id="login-form" onSubmit={this.handleLogin}>
+          <form id='login-form' onSubmit={this.handleLogin}>
             <FormGroup>
               <FormControl
-                type="text"
-                name="username"
-                placeholder="Username"
+                type='text'
+                name='username'
+                placeholder='Username'
                 autoFocus
                 required
                 onChange={this.handleChange}
               />
-              <i className="fas fa-user-alt"></i>
+              <i className='fas fa-user-alt'></i>
             </FormGroup>
             <FormGroup>
               <FormControl
-                type="password"
-                name="password"
-                placeholder="password"
+                type='password'
+                name='password'
+                placeholder='password'
                 required
                 onChange={this.handleChange}
               />
-              <i className="fas fa-lock"></i>
+              <i className='fas fa-lock'></i>
             </FormGroup>
             <div>
               <FormText>
-                <Link to="/register-form"> New User? Sign Up</Link>
+                <Link to='/register-form'> New User? Sign Up</Link>
               </FormText>
             </div>
             {/* <GoogleLogin
@@ -96,15 +96,15 @@ class LoginForm extends React.Component {
             />
             , */}
             <Button
-              type="submit"
-              variant="outline-primary"
-              className="mt-2"
+              type='submit'
+              variant='outline-primary'
+              className='mt-2'
               disabled={loading}
             >
               Login
             </Button>
-            {loading && <Spinner name="circle" color="blue" />}
-            {error && <p style={{ color: "red" }}>{error.message}</p>}
+            {loading && <Spinner name='circle' color='blue' />}
+            {error && <p style={{ color: 'red' }}>{error.message}</p>}
           </form>
         </Container>
       </section>
