0f2cd86e9cbe8b212a6e7168eadb2d247df715bf
David Guzman <davidguzmaninc@gmail.com>
2021-02-06 18:14:39 -0500

    Updates

---
+++ precache-manifest.3726b6a8556b9edc8daa2ac14a047eda.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b46283fda2180c791aa06d9b96f1fae",
+    "url": "/daveguz97/index.html"
+  },
+  {
+    "revision": "642dfbc48aa43bff18a0",
+    "url": "/daveguz97/static/css/2.d97600f8.chunk.css"
+  },
+  {
+    "revision": "177270d84d8fe5ef91af",
+    "url": "/daveguz97/static/css/main.cb1f1e7b.chunk.css"
+  },
+  {
+    "revision": "642dfbc48aa43bff18a0",
+    "url": "/daveguz97/static/js/2.2a29385b.chunk.js"
+  },
+  {
+    "revision": "a077b115842b665c7b7a172d0528f8df",
+    "url": "/daveguz97/static/js/2.2a29385b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "177270d84d8fe5ef91af",
+    "url": "/daveguz97/static/js/main.9ad9c41e.chunk.js"
+  },
+  {
+    "revision": "9a03f6322b7e8610e8b6",
+    "url": "/daveguz97/static/js/runtime-main.6b8a0052.js"
+  },
+  {
+    "revision": "77f5794e2eb49f7989b8f85e92cfa4e0",
+    "url": "/daveguz97/static/media/blank-profile-pic.77f5794e.png"
+  },
+  {
+    "revision": "801ae5b867f30210eca8dcfde18e406a",
+    "url": "/daveguz97/static/media/kwitter-logo.801ae5b8.png"
+  },
+  {
+    "revision": "7d0dd93ae10a3c55c2bf8dd86ed0ec05",
+    "url": "/daveguz97/static/media/login-background.7d0dd93a.jpeg"
+  }
+]);
