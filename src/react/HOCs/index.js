908b0390545a61c8e738bbe767128924950b48fc
David Guzman <davidguzmaninc@gmail.com>
2021-02-06 18:19:08 -0500

    Updates

--- src/react/HOCs/index.js
+++
@@ -1,3 +0,0 @@
-export { default as userIsAuthenticated } from "./userIsAuthenticated";
-export { default as userIsNotAuthenticated } from "./userIsNotAuthenticated";
-export { connect } from "react-redux";
